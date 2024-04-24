57223e1bee5e8e7982a4b1cedbd908f01ac2ff83
Megan Price <price.megan27@gmail.com>
2024-04-24 15:05:10 -0700

    Added html to app component and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import deliveryScooter from "./images/deliveryScooter.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <h1>Crust Crew</h1>
+       <img src={deliveryScooter}></img>
        <h3>Delicious neopolitan pizza delivered straight to your door!</h3>
+       <button>ORDER NOW</button>
       </header>
     </div>
   );
