179b814cb083a8027096342be2160529835a372d
Megan Price <price.megan27@gmail.com>
2024-04-24 12:41:17 -0700

    Initialising react app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Crust Crew</h1>
+       <h3>Delicious neopolitan pizza delivered straight to your door!</h3>
       </header>
     </div>
   );
