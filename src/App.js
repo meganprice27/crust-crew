acbeb4ef65c748ceb9ef9a15a5955fc4b7bf4647
Megan Price <price.megan27@gmail.com>
2024-04-25 16:51:31 -0700

    Added dummy text and styling to menu component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
        <h1>Crust Crew</h1>
        <img src={deliveryScooter} alt="Pizza delivery person riding on a scooter" className="homePageImage"></img>
-       <h3>Delicious neopolitan pizza delivered straight to your door!</h3>
+       <h4>Delicious neopolitan pizza delivered straight to your door!</h4>
        <button>ORDER NOW</button>
       </header>
     </div>
--- src/Menu.js
+++ src/Menu.js
@@ -1,9 +1,42 @@
-import './Menu.css'
+import './Menu.css';
+import pizzaBox from './images/pizzaBox.png';
 
 function Menu() {
     return (
-        <div className="menuContainer">
-           Hello World! 
+        <div className="menuBackground">
+           <div className="menuContainer"> 
+            <div>
+                <img src={pizzaBox} alt="Pizza box" className="pizzaBoxImage"></img>
+            </div>
+            <h2> 
+                Menu
+            </h2>
+            <h3>
+                Starters & sharers
+            </h3>
+            <h5>
+                Burrata (v)
+            </h5>
+            <div className="row menuItem">
+                <div className="col-7 itemDescription">
+                    Whole burrata, vine tomatoes, wild rocket and our house baked bread.
+                </div>
+                <div className="col plusMinus">
+                    <div>
+                    <i class="fa-solid fa-circle-minus"></i>
+                    </div>
+                    <div>
+                        0
+                    </div>
+                    <div>
+                    <i class="fa-solid fa-circle-plus"></i>
+                    </div>
+                </div>
+                <div className="col itemPrice">
+                    $11
+                </div>
+            </div>
+           </div>
         </div>
     );
 }
